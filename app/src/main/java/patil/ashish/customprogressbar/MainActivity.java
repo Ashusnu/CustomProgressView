cb21e453cfc056fd429afa56b6a66ad8aebcc81b
ashish.patil <>
2019-08-22 18:15:11 +0530

    Disk Ring Progress Bar Added Line Progress Bar Added

--- app/src/main/java/patil/ashish/customprogressbar/MainActivity.java
+++ app/src/main/java/patil/ashish/customprogressbar/MainActivity.java
@@ -3,6 +3,7 @@ package patil.ashish.customprogressbar;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import patil.ashish.customprogressview.CustomProgressView;
 import patil.ashish.customprogressview.TEXT_SIZE;
@@ -10,7 +11,7 @@ import patil.ashish.customprogressview.TEXT_SIZE;
 public class MainActivity extends AppCompatActivity {
 
 
-    CustomProgressView customProgressView;
+    CustomProgressView customProgressView,customProgressView1,customProgressView2;
     private boolean mStopHandler = false;
     private int p = 0;
     private Handler mHandler;
@@ -22,17 +23,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         customProgressView = findViewById(R.id.custom_progress);
-        customProgressView.setTextSize(TEXT_SIZE.MEDIUM);
+        customProgressView.setProgressType(CustomProgressView.TYPE.BASIC_CIRCULAR);
+        customProgressView.setTextSize(TEXT_SIZE.SMALL);
         customProgressView.setTextColor(ContextCompat.getColor(this,R.color.progress_color));
+
+        customProgressView1 = findViewById(R.id.custom_progress1);
+        customProgressView1.setProgressType(CustomProgressView.TYPE.DISK_RING);
+        customProgressView1.setTextSize(TEXT_SIZE.SMALL);
+        customProgressView1.setTextColor(ContextCompat.getColor(this,R.color.progress_color));
+
+
+        customProgressView2 = findViewById(R.id.custom_progress2);
+        customProgressView2.setProgressType(CustomProgressView.TYPE.LINE_PROGRESS);
+        customProgressView2.setTextSize(TEXT_SIZE.MEDIUM);
+        customProgressView2.setTextColor(ContextCompat.getColor(this,R.color.progress_color));
+
+
+
+        SwipeRefreshLayout refresh = findViewById(R.id.refresh);
+
+        refresh.setOnRefreshListener(() -> {
+            runProgressView();
+            refresh.setRefreshing(false);
+        });
 
 
         /*
+
             you can user below methods to set custom colors
          customProgressView.setProgressBackgroundColor(ContextCompat.getColor(this,R.color.light_gray));
          customProgressView.setProgressColor(ContextCompat.getColor(this,R.color.green));
          */
+        runProgressView();
+    }
 
 
+    private void runProgressView() {
+        p = 0;
+        mStopHandler = false;
+        // reset progress view
+        customProgressView.resetProgress();
+        customProgressView1.resetProgress();
+        customProgressView2.resetProgress();
 
         mHandler = new Handler();
         Runnable runnable = new Runnable() {
@@ -44,6 +76,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 customProgressView.setProgress(p);
                 customProgressView.setCenterText(String.valueOf(p));
+
+                customProgressView1.setProgress(p);
+                customProgressView1.setCenterText(String.valueOf(p));
+
+                customProgressView2.setProgress(p);
+                customProgressView2.setCenterText(String.valueOf(p));
+
                 if (!mStopHandler) {
                     mHandler.post(this);
                 }
@@ -52,4 +91,8 @@ public class MainActivity extends AppCompatActivity {
 
         mHandler.post(runnable);
     }
+
 }
+
+
+
--- customprogressview/src/main/java/patil/ashish/customprogressview/CustomProgressView.java
+++ customprogressview/src/main/java/patil/ashish/customprogressview/CustomProgressView.java
@@ -12,13 +12,18 @@ public class CustomProgressView extends View {
 
     public enum TYPE {
         BASIC_CIRCULAR,
-        CIRCULAR_WITH_CENTER_TEXT
+        DISK_RING,
+        LINE_PROGRESS
     }
 
     private Paint back_circlePaint = new Paint();
     private Paint progress_arc = new Paint();
     private Paint bullet_paint = new Paint();
     private Paint center_txtPaint = new TextPaint();
+    private Paint circular_disk_bg_paint = new Paint();
+
+    private Paint progress_line_paint = new Paint();
+    private Paint progress_line_bg_paint = new Paint();
     private RectF rectF = new RectF();
     private double progress;
     double one_degree = 3.6;
@@ -29,9 +34,10 @@ public class CustomProgressView extends View {
     private float handleRadius;
     private float textHeight;
     private float textOffset;
-    private int arc_colour = Color.parseColor("#F50057");
-    private int arc_bg_colour = Color.parseColor("#FCE4EC");
+    private int progress_color = Color.parseColor("#F50057");
+    private int progress_bg_color = Color.parseColor("#FCE4EC");
     private int textColor = Color.parseColor("#F50057");
+    private TYPE current_pb_type = TYPE.BASIC_CIRCULAR;
 
     public CustomProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -50,16 +56,19 @@ public class CustomProgressView extends View {
 
     private void init(AttributeSet attributeSet) {
         back_circlePaint.setStrokeWidth(40f);
-        back_circlePaint.setColor(arc_bg_colour);
+        back_circlePaint.setColor(progress_bg_color);
         back_circlePaint.setStyle(Paint.Style.STROKE);
 
+        circular_disk_bg_paint.setColor(progress_bg_color);
+        circular_disk_bg_paint.setStyle(Paint.Style.FILL);
+
         progress_arc.setStrokeWidth(40f);
-        progress_arc.setColor(arc_colour);
+        progress_arc.setColor(progress_color);
         progress_arc.setStyle(Paint.Style.STROKE);
         progress_arc.setAntiAlias(true);
         progress_arc.setStrokeCap(Paint.Cap.ROUND);
 
-        bullet_paint.setColor(arc_colour);
+        bullet_paint.setColor(progress_color);
         bullet_paint.setStyle(Paint.Style.FILL);
         bullet_paint.setAntiAlias(true);
 
@@ -72,13 +81,45 @@ public class CustomProgressView extends View {
         textOffset = textHeight / 2 - center_txtPaint.descent();
 
 
+        progress_line_paint.setColor(progress_color);
+        progress_line_paint.setAntiAlias(true);
+        progress_line_paint.setStyle(Paint.Style.STROKE);
+        progress_line_paint.setStrokeCap(Paint.Cap.ROUND);
+        progress_line_paint.setStrokeWidth(40f);
+
+        progress_line_bg_paint.setColor(progress_bg_color);
+        progress_line_bg_paint.setAntiAlias(true);
+        progress_line_bg_paint.setStyle(Paint.Style.STROKE);
+        progress_line_bg_paint.setStrokeCap(Paint.Cap.ROUND);
+        progress_line_bg_paint.setStrokeWidth(40f);
+
+
         animator = ValueAnimator.ofInt(0, 100);
         animator.setInterpolator(new AccelerateDecelerateInterpolator());
     }
 
+
+    public void setProgressType(TYPE progressType) {
+        this.current_pb_type = progressType;
+    }
+
     public void setProgress(int progress) {
         animator.addUpdateListener(animation -> {
-            this.progress = one_degree * progress;
+          switch (current_pb_type) {
+                case BASIC_CIRCULAR:
+                    this.progress = one_degree * progress;
+                    break;
+
+                case DISK_RING:
+                    this.progress = one_degree * progress;
+                    break;
+
+                case LINE_PROGRESS:
+                    this.progress = progress;
+                    break;
+            }
+
+
             CustomProgressView.this.postInvalidate();
         });
         animator.start();
@@ -90,16 +131,17 @@ public class CustomProgressView extends View {
 
     public void resetProgress() {
         this.setProgress(0);
+        setCenterText(null);
     }
 
     public void setProgressColor(int progressColor) {
-        this.arc_colour = progressColor;
-        progress_arc.setColor(arc_colour);
+        this.progress_color = progressColor;
+        progress_arc.setColor(progress_color);
     }
 
     public void setProgressBackgroundColor(int progressBackgroundColor) {
-        this.arc_bg_colour = progressBackgroundColor;
-        back_circlePaint.setColor(arc_bg_colour);
+        this.progress_bg_color = progressBackgroundColor;
+        back_circlePaint.setColor(progress_bg_color);
     }
 
     public void setCenterText(String centerText) {
@@ -127,22 +169,30 @@ public class CustomProgressView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         this.canvas = canvas;
         float center_height = canvas.getHeight() / 2;
         float center_width = canvas.getWidth() / 2;
         float radius = canvas.getWidth()/3;
-        handleRadius = radius /10;
 
-        long p = Math.round(progress/3.6);
 
-        rectF.set(center_width - radius, center_height - radius,  center_width + radius, center_height + radius);
-        canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,radius,back_circlePaint);
-        canvas.drawArc(rectF,-90,(float) progress,false,progress_arc);
 
-        if (centerText != null) {
-            canvas.drawText(centerText,center_width,center_height + textOffset,center_txtPaint);
-        }
 
+        switch (current_pb_type) {
+            case BASIC_CIRCULAR:
+
+
+                handleRadius = radius /10;
+
+                long p = Math.round(progress/3.6);
+
+                rectF.set(center_width - radius, center_height - radius,  center_width + radius, center_height + radius);
+                canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,radius,back_circlePaint);
+                canvas.drawArc(rectF,-90,(float) progress,false,progress_arc);
+
+                if (centerText != null) {
+                    canvas.drawText(centerText,center_width,center_height + textOffset,center_txtPaint);
+                }
 
    /*     if (has_bullet) {
             double bullet_progress = progress/one_degree;
@@ -153,6 +203,48 @@ public class CustomProgressView extends View {
                     bullet_paint);
         }
 */
+
+                break;
+
+            case DISK_RING:
+
+
+                rectF.set(center_width - radius, center_height - radius,  center_width + radius, center_height + radius);
+                canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight()/2,radius,circular_disk_bg_paint);
+                canvas.drawArc(rectF,-90,(float) progress,false,progress_arc);
+
+                if (centerText != null) {
+                    canvas.drawText(centerText,center_width,center_height + textOffset,center_txtPaint);
+                }
+
+
+                break;
+
+            case LINE_PROGRESS:
+
+                int offset = 30;
+                float height = canvas.getHeight();
+                float width  = canvas.getWidth();
+                float one_percent = width/100;
+                float line_progress = (float) (progress * one_percent);
+
+                if (centerText != null) {
+                    canvas.drawText(centerText,width/2,(height/2) - progress_line_paint.getStrokeWidth() - offset  + offset,center_txtPaint);
+                }
+
+                canvas.drawLine(offset,
+                        height/2,width - offset,
+                        (height/2),
+                        progress_line_bg_paint);
+
+                canvas.drawLine(offset,
+                        height/2,line_progress - offset,
+                        (height/2),
+                        progress_line_paint);
+
+                break;
+        }
+
     }
 
 
